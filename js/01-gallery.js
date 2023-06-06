f27ff4e0472c706955069027caf9ec544354d771
Juan D Adames <juandadames@gmail.com>
2023-06-06 14:57:46 -0500

    tarea 7 correcciones

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,31 +1,33 @@
-import { galleryItems } from "./gallery-items.js";
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
 
-console.log(galleryItems);
+const listGallery = document.querySelector('.gallery');
+galleryItems.forEach((element) => {
+  listGallery.insertAdjacentHTML(
+    'beforeend',
+    `<li class="gallery__image"><a href="${element.original}" class="gallery__link"><img class="gallery__image" src="${element.preview}" alt="${element.description}</a></li>">`
+  );
+});
+var linkGallery = document.querySelectorAll('.gallery__link');
 
-const galleryList = document.querySelector(".gallery");
-
-const galleryGrid = galleryItems.map((element) => 
-`<li class= "gallery__item">
-<a class= "gallery__link" href = "${element.original}">
- <img 
-class= "gallery__image"
-src= "${element.preview}"
-data-source= "${element.original}"
-alt= "${element.description}"
-/>
-</a>
-</li>`
-)
-.join("");
-
-galleryList.insertAdjacentHTML("afterbegin" , galleryGrid);
-
-galleryList.querySelectorAll(".gallery__image").forEach((image) => {
-image.addEventListener("click", (event)=> {
-    event.preventDefault();
-    const imageURL = event.target.getAttribute("data-source");
-    const lightbox = basicLightbox.create(`<img src="${imageURL}">`);
-    lightbox.show();
-})
+linkGallery.forEach((element) => {
+  element.addEventListener('click', (e) => {
+    e.preventDefault();
+    var href = element.getAttribute('href');
+    var instance = basicLightbox.create(`
+  <div class="link-gallery">
+    <img class="link-gallery__img" src="${href}">
+  </div>
+  `);
+    instance.show(() => console.log('hola'));
+    const img = document.querySelectorAll('.link-gallery__img');
+    const backgroundLightBox = document.querySelector('.basicLightbox');
+    img.forEach((imgItem) => {
+      imgItem.addEventListener('mouseout', () => {
+        backgroundLightBox.style.cursor  = `url("data:image/svg+xml,%3Csvg width='25' height='25' viewBox='0 0 32 32' xmlns='http://www.w3.org/2000/svg'%3E%3Cg stroke='none' stroke-width='1' fill='none' fill-rule='evenodd'%3E%3Ccircle fill-opacity='.6' fill='%23FFF' cx='16' cy='16' r='16'/%3E%3Cpath fill='%23000' d='M20.385 11l-4.395 4.39-4.375-4.37-.615.615L15.375 16 11 20.37l.615.61 4.375-4.365L20.385 21l.615-.61L16.604 16 21 11.615z'/%3E%3C/g%3E%3C/svg%3E") 16 16,pointer`;
+      });
+      imgItem.addEventListener('mouseover', () => {
+        backgroundLightBox.style.cursor = 'default';
+      });
+    });
+  });
 });
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,35 +1,14 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
-
-const galleryList = document.querySelector(".gallery");
-
-const galleryGrid = galleryItems.map((element) => 
-`<li class= "gallery__item">
-<a class= "gallery__link" href = "${element.original}">
-<img 
-class= "gallery__image"
-src= "${element.preview}"
-alt= "${element.description}"
-/>
-</a>
-</li>`
-)
-.join("");
-
-galleryList.insertAdjacentHTML("afterbegin" , galleryGrid);
-
-galleryList.querySelectorAll(".gallery__image").forEach((image) => {
-    image.addEventListener("click", (event)=> {
-        event.preventDefault();
-        const imageURL = event.target.getAttribute("src");
-        const lightbox = basicLightbox.create(`<img src="${imageURL}">`);
-        lightbox.show();
-    });
-    });
-    
-    let lightBox = new SimpleLightbox('.gallery .gallery__link', {
-    captionsData: 'alt',
-    captionDelay: 250
-    });
+const listGallery = document.querySelector('.gallery');
+galleryItems.forEach((element) => {
+  listGallery.insertAdjacentHTML(
+    'beforeend',
+    `<li class="gallery__image"><a href="${element.original}" class="gallery__link"><img class="gallery__image" src="${element.preview}" alt="${element.description}"></a></li>`
+  );
+});
+let galleryCaption = new SimpleLightbox('.gallery__link', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
